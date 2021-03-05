dc8348395e313e22b590370bf93bdf4e2980b28c
Mike Jandreau <mikejandreau@gmail.com>
2021-03-05 15:39:58 -0500

    Added custom fields, editable in Theme Options

--- footer.php
+++ footer.php
@@ -20,31 +20,63 @@
 				<div class="col-sm-12 col-md-4 col-lg-3">
 					<div class="footer-block">
 						<h4>Digital Colby</h4>
-						<p>Digital Colby presents the intellectual, creative and scholarly culture of the Colby College Community.</p>
+						<?php if ($footerTagline = get_option('dcv3_options')['dcv3-footer-info']) { 
+							echo '<p>' . $footerTagline . '</p>';
+						} else { /* do nothing */ } ?>
+
 					</div>
 				</div>
 
 				<div class="col-sm-6 col-md-4 col-lg-3">
 					<div class="footer-block">
 						<h4>Browse</h4>
-						<ul>
-							<li><a href="#">Link to Something</a></li>
-							<li><a href="#">Link to Something</a></li>
-							<li><a href="#">Link to Something</a></li>
-							<li><a href="#">Link to Something</a></li>
-							<li><a href="#">Link to Something</a></li>
-						</ul>
+						<?php // footer menu
+							wp_nav_menu( 
+								array( 
+								'theme_location' => 'menu-2', 
+								'container'       => 'div',
+								'container_id'    => 'footer-menu-wrap',
+								'container_class' => 'footer-menu-wrap',
+								'menu_id'         => 'footer-menu',
+								'menu_class'      => 'footer-menu-ul' 
+								)
+							); 
+						?>
 					</div>
 				</div>
 
 				<div class="col-sm-6 col-md-4 col-lg-3">
 					<div class="footer-block">
 						<h4>Visit</h4>
-						<p><strong>Colby College Libraries</strong> <br>
-							5100 Mayflower Hill <br>
-						Waterville, ME 04901</p>
-						<a href="tel:207-859-5100"><i class="fas fa-phone"></i> 207-859-5100</a> <br>
-						<a href="mailto:libraries@colby.edu"><i class="fas fa-envelope"></i> libraries@colby.edu</a>
+						<p>
+							<?php if ($footerAddressName = get_option('dcv3_options')['dcv3-footer-address-name']) { 
+								echo '<span><strong>' . $footerAddressName. '</strong></span><br>';
+							} else { /* do nothing */ } ?>
+
+							<?php if ($footerStreetAddress = get_option('dcv3_options')['dcv3-footer-street-address']) { 
+								echo '<span>' . $footerStreetAddress. '</span><br>';
+							} else { /* do nothing */ } ?>
+
+							<?php if ($footerPoBox = get_option('dcv3_options')['dcv3-footer-po-box']) { 
+								echo '<span>' . $footerPoBox. '</span><br>';
+							} else { /* do nothing */ } ?>
+
+							<?php if ($footerCityStateZip = get_option('dcv3_options')['dcv3-footer-city-state-zip']) { 
+								echo '<span>' . $footerCityStateZip. '</span><br>';
+							} else { /* do nothing */ } ?>
+						</p>
+
+						<?php if ($footerPhone = get_option('dcv3_options')['dcv3-footer-phone']) { 
+							echo '<span><a href="tel:+1-' . $footerPhone . '"><i class="fas fa-phone"></i> ' . $footerPhone . '</a></span><br>';
+						} else { /* do nothing */ } ?>
+
+						<?php if ($footerFax = get_option('dcv3_options')['dcv3-footer-fax']) { 
+							echo '<span><a href="tel:+1-' . $footerFax . '"><i class="fas fa-fax"></i> ' . $footerFax . '</a></span><br>';
+						} else { /* do nothing */ } ?>
+
+						<?php if ($footerEmail = get_option('dcv3_options')['dcv3-footer-email']) { 
+							echo '<span><a href="mailto:' . $footerEmail . '"><i class="fas fa-envelope"></i> ' . $footerEmail . '</a><br></span>';
+						} else { /* do nothing */ } ?>
 					</div>
 				</div>
 
@@ -52,12 +84,32 @@
 					<div class="footer-block">
 						<h4>Connect</h4>
 						<div class="social-block">
-							<a id="instagram" title="Instagram" href="https://www.instagram.com/colbycollegelibraries/" target="_blank"><i class="fab fa-instagram fa-2x"></i></a>
-							<a id="twitter" title="Twitter" href="http://www.twitter.com/colbycollege" target="_blank"><i class="fab fa-twitter fa-2x"></i></a>
-							<a id="facebook" title="Facebook" href="http://www.facebook.com/colbycollege" target="_blank"><i class="fab fa-facebook-f fa-2x"></i></a>
-							<a id="youtube" title="YouTube" href="http://www.youtube.com/colbycollege" target="_blank"><i class="fab fa-youtube fa-2x"></i></a>
-							<a id="vimeo" title="Vimeo" href="http://vimeo.com/colbycollege" target="_blank"><i class="fab fa-vimeo-v fa-2x"></i></a>
-							<a id="rss" title="RSS" href="/news/?feed=rss" target="_blank"><i class="fas fa-rss fa-2x"></i></a>
+							<?php  // These fields are controlled using the theme options located in Appearance -> Theme Options
+
+							if ($instagram = get_option('dcv3_options')['dcv3-social-instagram']) { 
+								echo '<a id="instagram" class="footer-social-icon" title="Instagram" target="_blank" href="' . $instagram . '"><i class="fab fa-instagram fa-2x"></i></a>';
+							} else { /* do nothing */ }
+							
+							if ($twitter = get_option('dcv3_options')['dcv3-social-twitter']) { 
+								echo '<a id="twitter" class="footer-social-icon" title="Twitter" target="_blank" href="' . $twitter . '"><i class="fab fa-twitter fa-2x"></i></a>';
+							} else { /* do nothing */ }
+
+							if ($facebook = get_option('dcv3_options')['dcv3-social-facebook']) { 
+								echo '<a id="facebook" class="footer-social-icon" title="Facebook" target="_blank" href="' . $facebook . '"><i class="fab fa-facebook-f fa-2x"></i></a>';
+							} else { /* do nothing */ }
+
+							if ($youtube = get_option('dcv3_options')['dcv3-social-youtube']) { 
+								echo '<a id="youtube" class="footer-social-icon" title="YouTube" target="_blank" href="' . $youtube . '"><i class="fab fa-youtube fa-2x"></i></a>';
+							} else { /* do nothing */ }
+
+							if ($vimeo = get_option('dcv3_options')['dcv3-social-vimeo']) { 
+								echo '<a id="vimeo" class="footer-social-icon" title="Vimeo" target="_blank" href="' . $vimeo . '"><i class="fab fa-vimeo-v fa-2x"></i></a>';
+							} else { /* do nothing */ }
+
+							if ($rssfeed = get_option('dcv3_options')['dcv3-social-rss']) { 
+								echo '<a id="rss" class="footer-social-icon" title="RSS" target="_blank" href="' . $rssfeed . '"><i class="fas fa-rss fa-2x"></i></a>';
+							} else { /* do nothing */ }
+							?>
 						</div>
 					</div>
 				</div>
